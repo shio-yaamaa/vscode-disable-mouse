3ccf0196298463684bc1cb7a6a4ec19dddac8b6e
YaaMaa <shio.yama520@gmail.com>
2022-04-10 22:06:08 +0900

    Implement extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,38 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { Timer } from './timer';
+
+let previousTextEditor: vscode.TextEditor | null = null;
+let previousSelections: vscode.Selection[] = [];
+
+const timer = new Timer(1000);
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "disable-mouse" is now active!');
+  vscode.window.onDidChangeTextEditorSelection((event) => {
+    const isAllowed = isSelectionChangeEventAllowed(
+      event,
+      previousTextEditor,
+      timer
+    );
+
+    if (isAllowed) {
+      previousTextEditor = event.textEditor;
+      previousSelections = event.selections.map((selection) => selection);
+    } else {
+      event.textEditor.selections = previousSelections;
+    }
+  });
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
-    'disable-mouse.helloWorld',
+    'disable-mouse.enable-mouse',
     () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage('Hello World from Disable Mouse!');
+      timer.set(3000);
     }
   );
 
@@ -26,3 +41,13 @@ export function activate(context: vscode.ExtensionContext) {
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
+
+const isSelectionChangeEventAllowed = (
+  event: vscode.TextEditorSelectionChangeEvent,
+  previousTextEditor: vscode.TextEditor | null,
+  timer: Timer
+): boolean => {
+  const isMouse = event.kind === vscode.TextEditorSelectionChangeKind.Mouse;
+  const isSameTextEditor = event.textEditor === previousTextEditor;
+  return !isMouse || !isSameTextEditor || !timer.hasEnded;
+};
---
+++ src/timer.ts
@@ -0,0 +1,34 @@
+export class Timer {
+  private time: number;
+  private timeoutId: NodeJS.Timeout | null;
+  private interval: number;
+
+  constructor(interval: number) {
+    this.time = 0;
+    this.timeoutId = null;
+    this.interval = interval;
+  }
+
+  public get hasEnded(): boolean {
+    return this.time <= 0;
+  }
+
+  public set(time: number) {
+    this.time = Math.max(this.time, time);
+    this.clear();
+    this.countDown();
+  }
+
+  private clear() {
+    if (this.timeoutId !== null) {
+      clearTimeout(this.timeoutId);
+    }
+  }
+
+  private countDown() {
+    this.timeoutId = setTimeout(() => {
+      this.time -= 1000;
+      this.countDown();
+    }, this.interval);
+  }
+}
