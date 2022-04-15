e7707a22303fad5c36e2098a14c0864c45ef0790
YaaMaa <shio.yama520@gmail.com>
2022-04-15 12:38:42 +0900

    Add and edit contributions

--- src/extension.ts
+++ src/extension.ts
@@ -30,7 +30,7 @@ export function activate(context: vscode.ExtensionContext) {
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
-    'disable-mouse.enable-mouse',
+    'disableMouse.enableMouse',
     () => {
       timer.set(3000);
     }
